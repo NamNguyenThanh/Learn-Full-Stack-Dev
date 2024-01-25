677aad13316bcdeab43486671f19b50120c0fb75
namnt <ntnam1510@gmail.com>
2024-01-25 14:33:39 +0700

    [Add] API for register new User

--- Realtime-Chat-App/server/api/v1/controllers/user.controller.js
+++ Realtime-Chat-App/server/api/v1/controllers/user.controller.js
@@ -1,7 +1,28 @@
 "use strict";
+const userService = require("../services/user.service");
+const createError = require("http-errors");
+
 module.exports.register = async (req, res, next) => {
   try {
-    console.log("Register user:::", req.body);
+    const { username, email, password } = req.body;
+    // TODO: Validate input using Joi or mongoose validation
+
+    // Check username or email was register
+    if (await userService.getUser({ username })) {
+      throw createError.Conflict("Username already used");
+    }
+    if (await userService.getUser({ email })) {
+      throw createError.Conflict("Email already used");
+    }
+
+    // Create new user
+    const user = await userService.createUser({ username, email, password });
+    delete user.password;
+
+    return res.json({
+      status: 200,
+      user,
+    });
   } catch (error) {
     next(error);
   }
