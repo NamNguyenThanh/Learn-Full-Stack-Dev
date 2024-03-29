9479b77d3d3948762ae32b35219652341a1bca71
namnt <ntnam1510@gmail.com>
2024-03-29 15:26:26 +0700

    [Mod] Change layout product list

--- Ecomerce/xb-sport/src/components/ProductList/index.js
+++ Ecomerce/xb-sport/src/components/ProductList/index.js
@@ -17,32 +17,34 @@ export default function ProductList({ productGroup }) {
       <ul className="row row-cols-5 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 g-2">
         {productGroup.products.map((product, index) => {
           return (
-            <li key={index} className="col product-card">
-              <a href={product.href}>
-                <img
-                  src={
-                    product.thumbnail
-                      ? productsImages('./noDefaultImage6_large.webp')
-                      : productsImages('./noDefaultImage6_large.webp')
-                  }
-                  alt=""
-                  className="product-card__thumb"
-                />
-                <div className="product-card__title">{product.name}</div>
-                <ul>
-                  {product.attributes.map((attribute, index) => {
-                    return (
-                      <li key={index} className="attr_item">
-                        <div>{`+${attribute.values.length} ${attribute.name}`}</div>
-                      </li>
-                    );
-                  })}
-                </ul>
-                <div className="product-card__price">
-                  <div className="price">{product.price}</div>
-                  <div className="ori_price">{product.ori_price}</div>
-                </div>
-              </a>
+            <li key={index} className="col">
+              <div className="product-card">
+                <a href={product.href}>
+                  <img
+                    src={
+                      product.thumbnail
+                        ? productsImages('./noDefaultImage6_large.webp')
+                        : productsImages('./noDefaultImage6_large.webp')
+                    }
+                    alt=""
+                    className="product-card__thumb"
+                  />
+                  <div className="product-card__title">{product.name}</div>
+                  <ul>
+                    {product.attributes.map((attribute, index) => {
+                      return (
+                        <li key={index} className="attr_item">
+                          <div>{`+${attribute.values.length} ${attribute.name}`}</div>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <div className="product-card__price">
+                    <div className="price">{product.price}</div>
+                    <div className="ori_price">{product.ori_price}</div>
+                  </div>
+                </a>
+              </div>
             </li>
           );
         })}
